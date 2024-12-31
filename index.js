9eeb01788f301ca4cf296df7ea17c2388913a3f5
BhargavMaru07 <bhargavhamir@gmail.com>
2024-12-31 14:57:57 +0530

    update route

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   database: "sigma",
-  password: process.env.PASSWORD,
+  password: process.env.PASSWORD || "Bhargav2005",
 });
 
 app.get("/", (req, res) => {
@@ -68,19 +68,38 @@ app.get("/user/:id/edit", (req, res) => {
 
 //update data
 app.patch("/user/:id", (req, res) => {
-  res.send("updated");
-  // let {id} = req.params
-  // let q = `SELECT * FROM user WHERE id = '${id}' `
+  let { id } = req.params;
+  let { password: formPass, username: newUserName } = req.body;
 
-  // try {
-  //     connection.query(q,(err,result)=>{
-  //         if(err) throw err
-  //         let user = result[0]
-  //         res.render("edit",{user})
-  //     })
-  // } catch (error) {
-  //     console.log(error);
-  // }
+  let q = "SELECT * FROM user WHERE id = ?";
+  connection.query(q, [id], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    if (result.length === 0) {
+      res.status(404).send("User not found");
+      return;
+    }
+
+    const user = result[0];
+
+    if (formPass !== user.password) {
+      res.status(401).send("WRONG PASSWORD");
+    } else {
+      let q2 = "UPDATE user SET username = ? WHERE id = ?";
+      connection.query(q2, [newUserName, id], (err, updateResult) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send("Internal Server Error");
+          return;
+        }
+        res.redirect("/user");
+      });
+    }
+  });
 });
 
 app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
